0a37ddeb9d4b64327e234a0d03d33bd3eed5242c
dysk0zero <javiroldan98@gmail.com>
2025-09-25 18:09:59 +0000

    Middleware working

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,16 @@
-import { defineMiddleware } from "astro/middleware";
+import { defineMiddleware } from 'astro:middleware';
 
-export const onRequest = defineMiddleware(async (context, next) => {
+const SUPPORTED_LOCALES = ['es', 'de'];
+
+export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
   const pathname = url.pathname;
 
-  if (pathname.startsWith("/es") || pathname.startsWith("/de")) {
-    return next();
-  }
+  const acceptLang = context.request.headers.get("accept-language") ?? "";
+  const preferred = acceptLang.split(",")[0].split("-")[0];
 
-  const acceptLanguage = context.request.headers.get("accept-language");
-  const preferredLang = acceptLanguage?.split(",")[0].split("-")[0];
-
-  if (preferredLang === "es" || preferredLang === "de") {
-    return Response.redirect(`${url.origin}/${preferredLang}${pathname}`, 302);
+  if (SUPPORTED_LOCALES.includes(preferred)) {
+    return context.redirect(`/${preferred}/`);
   }
 
   return next();
